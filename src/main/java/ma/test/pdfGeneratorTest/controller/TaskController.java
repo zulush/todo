21d9720407b87bd96547b627ca3a597ae5ae16f2
Zouizza <szouizza@gmail.com>
2022-11-29 17:00:50 +0100

    creating-new-Task-feature-post-request

--- src/main/java/ma/test/pdfGeneratorTest/controller/TaskController.java
+++ src/main/java/ma/test/pdfGeneratorTest/controller/TaskController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +34,7 @@ public class TaskController {
 	}
 	
 	
-	@GetMapping("/personal/new")
+	@PostMapping("/personal")
 	public ResponseEntity<Object> addTask(@RequestBody Task newTask) {
 		
 		try {
